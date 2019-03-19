61dec22a531fffeabb9f1f35d93bc62f0da5585a
zyhxj <j15874294361@126.com>
2019-03-19 11:53:25 +0800

    第二次

--- src/main/java/com/hr/springboot/ServletInitializer.java
+++ src/main/java/com/hr/springboot/ServletInitializer.java
@@ -3,11 +3,14 @@ package com.hr.springboot;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javax.swing.*;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringbootApplication.class);
+
     }
 
 }
